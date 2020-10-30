6edfa977f86b675d0bed843e3a2558980c290d6d
anshumandixit9 <anshumandixit7@gmail.com>
2020-10-30 13:28:50 +0530

    search bar

--- src/SideBar.js
+++ src/SideBar.js
@@ -2,21 +2,34 @@ import React, {Component} from 'react';
 import './SideBar.css';
 import SideNav, {NavItem, NavIcon, NavText} from '@trendmicro/react-sidenav';
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
-
 const data = null;
+
+function searchingFor(search){
+    return function(x){
+        return x.name.toLowerCase().includes(search.toLowerCase()) || !search;
+    }
+}
 
 export class SideBar extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.data = require('./shared/data.json')
+        this.state ={
+            allData :this.data,
+            search : ''
+        }
+        this.searchHandler = this.searchHandler.bind(this);
+    }
+
+    searchHandler(event){
+        this.setState({search: event.target.value})
     }
 
     render() {
-
-        return (
+           return (
             <SideNav
-                onSelect={(selected) => {
+                        onSelect={(selected) => {
                     // Add your code here
                 }}>
                 <SideNav.Toggle/>
@@ -36,16 +49,21 @@ export class SideBar extends Component {
                         <NavText>
                             List
                         </NavText>
+                        <NavItem>
+                            <NavText>
+                                <input type="text" placeholder="Search" class="searchbox" onChange={this.searchHandler} value={this.state.search}/>
+                            </NavText>
+                        </NavItem>
                         {
-                            this.data.map(element => {
-                                return (
-                                    <NavItem eventKey={element.name}>
-                                        <NavText>
-                                            {element.name}
-                                        </NavText>
-                                    </NavItem>
-                                )
-                            })
+                            this.state.allData.filter(searchingFor(this.state.search)).map(element => {
+                            return (
+                                <NavItem eventKey={element.name}>
+                                    <NavText>
+                                        {element.name}
+                                    </NavText>
+                                </NavItem>
+                            )
+                        })
                         }
                     </NavItem>
                 </SideNav.Nav>
---
+++ src/shared/sample_data.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Nav (){
+    return(
+        <div>
+            <h1>
+                Something Better than Nothing!!
+            </h1>
+        </div>
+    );
+}
+
+export default Nav;
+
+
